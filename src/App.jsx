7977e092622b4b9b45964d80ff0eaae40b63f79f
Viktor Zvarych <viktor.zvarych@gmail.com>
2023-10-28 12:10:54 +0300

    add footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Navbar from './Components/Navbar/Navbar'
 import MainContent from './Components/MainContent/MainContent'
+import Footer from './Components/Footer/Footer'
 import './App.css'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="card">
       <Navbar />
       <MainContent />
+      <Footer />
     </div>
   )
 }
